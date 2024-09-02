5c6eff8db12c2564cff6d8591de7be3ebff9e487
Searhei <156844838+serg7527@users.noreply.github.com>
2024-09-02 23:08:37 +0300

    005

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask_login import LoginManager, UserMixin, login_user, login_required, log
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'  # Замените на свой секретный ключ
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ads.db'
@@ -13,7 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
-
 
 
 @login_manager.user_loader
@@ -32,7 +30,6 @@ class Ad(db.Model):
     title = db.Column(db.String(200), nullable=False)
     description = db.Column(db.Text, nullable=False)
     image_filename = db.Column(db.String(200), nullable=True)
-
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -56,7 +53,6 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html')
 
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -70,23 +66,19 @@ def login():
             flash('Неверные учетные данные. Пожалуйста, попробуйте снова.', 'danger')
     return render_template('login.html')
 
-
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('index'))
 
-
 @app.route('/', methods=['GET'])
 def index():
     ads = Ad.query.all()  # Извлекаем все объявления из базы данных
     return render_template('index.html', ads=ads)
 
-
 UPLOAD_FOLDER = 'static/uploads'  # Папка для сохранения загруженных изображений
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
 
 @app.route('/add_ad', methods=['GET', 'POST'])
 @login_required
@@ -111,7 +103,31 @@ def add_ad():
 
     return render_template('add_ad.html')
 
+@app.route('/edit_ad/<int:ad_id>', methods=['GET', 'POST'])
+@login_required
+def edit_ad(ad_id):
+    ad = Ad.query.get_or_404(ad_id)
 
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        image = request.files['image']
+
+        # Сохранение изображения на сервере
+        image_filename = ad.image_filename  # Оставляем текущее, если новое не загружено
+        if image:
+            image_filename = image.filename
+            image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_filename))
+
+        ad.title = title
+        ad.description = description
+        ad.image_filename = image_filename
+
+        db.session.commit()
+        flash('Объявление успешно обновлено!', 'success')
+        return redirect(url_for('index'))
+
+    return render_template('edit_ad.html', ad=ad)
 
 if __name__ == '__main__':
     with app.app_context():
